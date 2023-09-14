863f0a29201b1034f5872e0b79b5fc21e6f6261b
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-09-14 10:46:36 +0530

    Add obscuring

---
+++ Demo36.java
@@ -0,0 +1,20 @@
+public class Demo36 {
+    public static void main(String[] args) {
+        // Shadowing
+        // Hiding
+        // Method Overloading
+        // Method Overriding
+        // Obscuring
+
+        // When obscuring happens in java, it uses a ambiguity resolving algorithm
+
+        // int String = 10;
+        // int System = 10;
+        // System.out.println(String.format("%s", "kasun"));
+        new mypackage.Customer();
+    }
+}
+
+class mypackage {
+    
+}
